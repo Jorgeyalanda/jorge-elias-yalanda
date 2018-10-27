16b90b0c966c4e3b602763a91066b1e363d0d570
jorge yalanda <jorgeyacalambas@gmail.com>
2018-10-27 17:37:16 -0500

    esta es la clase del sabado 27/10/18

---
+++ public/cv/js/funciones.js
@@ -0,0 +1,19 @@
+function suma(a, b) {
+    console.log(a + b);
+}
+function mostrarvalor() {
+    alert(campotex.value);
+}
+ver lugar = "r"
+function moverboton() {
+    boton.className= "ui button left floated";
+    lugar = "l";
+}
+if (lugar=="l"){
+    boton.className= "ui button right floader";
+    lugar = "r"
+}
+else if lugar=="l"){
+    boton.className= "ui button right floader";
+    lugar = "r"
+}
